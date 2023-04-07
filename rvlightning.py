61b74786b4211340911b4fc1a3d1361eb6dace5d
Jack Wolf <jackw@aerotractone.com>
2023-04-07 09:07:43 -0700

    working

--- rvlightning.py
+++ rvlightning.py
@@ -1,60 +1,86 @@
 import albumentations as A
-from rastervision.pytorch_learner import (
-    SemanticSegmentationRandomWindowGeoDataset,
-    SemanticSegmentationSlidingWindowGeoDataset,
-)
 from rastervision.core.data import ClassConfig
-from tqdm.autonotebook import tqdm
-import torch
-from torch.utils.data import DataLoader
-from torch.nn import functional as F
-from torchvision.models.segmentation import deeplabv3_resnet50
-import pytorch_lightning as pl
+from rastervision.pytorch_learner import (
+    ObjectDetectionRandomWindowGeoDataset,
+    ObjectDetectionSlidingWindowGeoDataset,
+)
+from rastervision.pytorch_learner.object_detection_utils import (
+   TorchVisionODAdapter, compute_coco_eval, collate_fn, BoxList)
+from rastervision.core.data.label.object_detection_labels import ObjectDetectionLabels
 from rastervision.pipeline.file_system import make_dir
-from pytorch_lightning.loggers import CSVLogger, TensorBoardLogger
-from rastervision.core.data import SemanticSegmentationLabels
-from rastervision.core.data.label_store.semantic_segmentation_label_store_config import PolygonVectorOutputConfig
-import pathlib
-import geopandas
-import rasterio
-from rasterio.features import shapes
-from smart_open import open as smart_open
+from torch.utils.data import DataLoader
+from torchvision.models import detection
+import torch
+import torch.nn.functional as F
+import pytorch_lightning as pl
+from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from tqdm import tqdm
+from smart_open import smart_open
 import io
-import boto3
+import pathlib 
 
-class SemanticSegmentation(pl.LightningModule):
-    
-    def __init__(self, deeplab, lr=1e-4):
+def exit():
+    import sys
+    sys.exit(0)
+
+class ObjectDetection(pl.LightningModule):
+
+    def __init__(self, backbone, lr=1e-4):
         super().__init__()
-        self.deeplab = deeplab
+        self.backbone = TorchVisionODAdapter(backbone)
         self.lr = lr
 
     def forward(self, img):
-        return self.deeplab(img)['out']
+        return self.backbone(img)
+    
+    def training_step(self, batch, batch_ind):
+        x, y = batch
+        loss_dict = self.backbone(x, y)
+        loss_dict['loss'] = sum(loss_dict.values())
+        self.log("train_loss", loss_dict["loss"])
+        return loss_dict
 
-    def training_step(self, batch, batch_idx):
-        img, mask = batch
-        img = img.float()
-        mask = mask.long()
-        out = self.forward(img)
-        loss = F.cross_entropy(out, mask)
-        log_dict = {'train_loss': loss}
-        self.log_dict(log_dict, on_step=True, on_epoch=True, prog_bar=True, logger=True)
-        return loss
+    def validate_step(self, batch, batch_ind):
+        x, y = batch
+        outs = self.backbone(x)
+        ys = self.to_device(y, 'cpu')
+        outs = self.to_device(outs, 'cpu')
+        print(outs)
+        return {'ys': ys, 'outs': outs}
 
-    def validation_step(self, batch, batch_idx):
-        img, mask = batch
-        img = img.float()
-        mask = mask.long()
-        out = self.forward(img)
-        loss = F.cross_entropy(out, mask)
-        log_dict = {'validation_loss': loss}
-        self.log_dict(log_dict, on_step=True, on_epoch=True, prog_bar=True, logger=True)
-        return loss
+    def validate_end(self, outputs):
+        outs = []
+        ys = []
+        for o in outputs:
+            outs.extend(o['outs'])
+            ys.extend(o['ys'])
+        num_class_ids = len(self.cfg.data.class_names)
+        coco_eval = compute_coco_eval(outs, ys, num_class_ids)
+        metrics = {'mAP': 0.0, 'mAP50': 0.0}
+        if coco_eval is not None:
+            coco_metrics = coco_eval.stats
+            metrics = {'val_mAP': coco_metrics[0], 'val_mAP50': coco_metrics[1]}
+            self.log_dict(metrics)
+        return metrics
+    
+    def predict(self, x, raw_out=False, out_shape=None):
+        self.backbone.eval()
+        with torch.no_grad():
+            out_batch = self.backbone(x)
+        if out_shape is None:
+            return out_batch
+        h_in, w_in = x.shape[-2:]
+        h_out, w_out = out_shape
+        yscale, xscale = (h_out / h_in), (w_out / w_in)
+        with torch.inference_mode():
+            for out in out_batch:
+                out.scale(yscale, xscale)
+        return out_batch
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(
-            self.parameters(), lr=self.lr)
+            self.backbone.parameters(), lr=self.lr)
         return optimizer
     
 class RVLightning:
@@ -72,13 +98,8 @@ class RVLightning:
         self.kw = kw
         
     def build_train_ds(self):
-        kw = self.kw.get("train_ds_kw", {})
-        data_augmentation_transform = A.Compose([
-            A.Flip(),
-            A.ShiftScaleRotate(),
-            A.RGBShift()
-        ])
-        train_ds = SemanticSegmentationRandomWindowGeoDataset.from_uris(
+        kw = self.kw.get("train_data_kw", {})
+        train_ds = ObjectDetectionRandomWindowGeoDataset.from_uris(
             class_config=self.cc,
             image_uri=self.train_uris[0],
             aoi_uri=self.train_uris[2],
@@ -86,13 +107,13 @@ class RVLightning:
             label_vector_default_class_id=self.cc.get_class_id('DF'),
             size_lims=kw.get("size_lims", [300,350]),
             out_size=kw.get("out_size", 325),
-            max_windows=kw.get("max_windows", 10),
-            transform=data_augmentation_transform)
+            max_windows=kw.get("max_windows", None),
+        )
         return train_ds
     
     def build_val_ds(self):
-        kw = self.kw.get("val_ds_kw", {})
-        val_ds = SemanticSegmentationSlidingWindowGeoDataset.from_uris(
+        kw = self.kw.get("val_data_kw", {})
+        val_ds = ObjectDetectionSlidingWindowGeoDataset.from_uris(
             class_config=self.cc,
             image_uri=self.val_uris[0],
             aoi_uri=self.val_uris[2],
@@ -103,9 +124,11 @@ class RVLightning:
         return val_ds
     
     def build_pred_ds(self):
-        kw = self.kw.get("pred_ds_kw", {})
+        kw = self.kw.get("pred_data_kw", {})
         def get_tiles_from_dir(d):
-            v = [x.as_posix() for x in pathlib.Path(d).glob("*.tif")]
+            v = []
+            for x in pathlib.Path(d).glob("*.tif"):
+                v.append(x.as_posix())
             return v
         def get_tiles_from_s3(p):
             s3 = boto3.resource("s3")
@@ -122,24 +145,48 @@ class RVLightning:
             img_uri = get_tiles_from_dir(self.pred_uris[0])
         if kw.get("num_images", None) is not None:
             img_uri = img_uri[:kw.get("num_images")]
-        pred_ds = SemanticSegmentationSlidingWindowGeoDataset.from_uris(
+        pred_ds = ObjectDetectionSlidingWindowGeoDataset.from_uris(
             class_config=self.cc,
             image_uri=img_uri,
-            aoi_uri=self.pred_uris[1],
             size=kw.get("size", 325),
             stride=kw.get("stride", 325))
         return pred_ds
     
-    def train(self):
-        kw = self.kw.get("train_kw", {})
-        batch_size = kw.get("batch_size", 8)
+    def build_train_val_loader(self):
+        tds, vds = self.build_train_ds(), self.build_val_ds()
+        tkw = self.kw.get("train_data_kw", {})
+        vkw = self.kw.get("val_data_kw", {})
+        train = DataLoader(tds, batch_size=tkw.get("batch_size", 2), shuffle=True, 
+                           num_workers=tkw.get("num_workers", 4), collate_fn=collate_fn)
+        val = DataLoader(vds, batch_size=vkw.get("batch_size", 2), 
+                         num_workers=vkw.get("num_workers", 4), collate_fn=collate_fn)
+        return train, val
+    
+    def build_pred_loader(self):
+        kw = self.kw.get("pred_data_kw", {})
+        pds = self.build_pred_ds()
+        pred_dl = DataLoader(pds, 
+                      batch_size=kw.get("batch_size", 2), num_workers=kw.get("num_workers", 4))
+        return pred_dl
+    
+    def model_type(self):
+        return getattr(detection, self.kw["model_kw"]["name"])
+    
+    def build_model(self):
+        kw = self.kw.get("model_kw", {})
         lr = float(kw.get("lr", 1e-4))
-        epochs = kw.get("epochs", 1)
         output_dir = self.output_uri
         make_dir(output_dir)
-        fast_dev_run = False
-        deeplab = deeplabv3_resnet50(num_classes=len(self.cc) + 1)
-        model = SemanticSegmentation(deeplab, lr=lr)
+        backbone = self.model_type()(
+            num_classes=len(self.cc)+1, pretrained=False)
+        model = ObjectDetection(backbone, lr=lr)
+        return model
+    
+    def train(self):
+        kw = self.kw.get("train_kw", {})
+        epochs = kw.get("epochs", 1)
+        model = self.build_model()
+        output_dir = self.output_uri
         tb_logger = TensorBoardLogger(save_dir=output_dir + "/tensorboard", flush_secs=10)
         trainer = pl.Trainer(
             accelerator='auto',
@@ -147,31 +194,43 @@ class RVLightning:
             max_epochs=epochs+1,
             default_root_dir=output_dir + "/trainer",
             logger=[tb_logger],
-            fast_dev_run=fast_dev_run,
+            fast_dev_run=False,
             log_every_n_steps=1,
+            callbacks=[EarlyStopping(monitor="train_loss", mode="min")],
         )
-        tds = self.build_train_ds()
-        vds = self.build_val_ds()
-        train_dl = DataLoader(tds, batch_size=batch_size, shuffle=True, num_workers=4)
-        val_dl = DataLoader(vds, batch_size=batch_size, num_workers=4)
+        train_dl, val_dl = self.build_train_val_loader()
         trainer.fit(model, train_dl, val_dl)
         trainer.save_checkpoint(output_dir + "/trainer/final-model.ckpt")
         
+    def output_to_numpy(self, out):
+        def boxlist_to_numpy(boxlist):
+            return {
+                'boxes': boxlist.convert_boxes('yxyx').numpy(),
+                'class_ids': boxlist.get_field('class_ids').numpy(),
+                'scores': boxlist.get_field('scores').numpy()
+            }
+
+        if isinstance(out, BoxList):
+            return boxlist_to_numpy(out)
+        else:
+            return [boxlist_to_numpy(boxlist) for boxlist in out]
+
     def prediction_iterator(self, pred_dl, model, n=None):
         for i, (x, _) in tqdm(enumerate(pred_dl)):
-            if n is not None and i >= n:
+            if n is not None and n != "None" and n > 0 and i >= n:
                 break
             with torch.inference_mode():
-                out_batch = model(x)
+                out_batch = model.predict(x)
+                out_batch = self.output_to_numpy(out_batch)
             for out in out_batch:
-                yield out.numpy()
-        
-    def predict(self):
-        kw = self.kw.get("predict_kw", {})
+                yield out
+                
+    def reconstruct_model(self):
         def fix_keys(data):
             for _ in range(len(data)):
                 k, v = data.popitem(False)
-                newk = k[k.index(".")+1:]
+                newk = k.split(".")
+                newk = ".".join(newk[2:])
                 data[newk] = v
             return data
         ckpt_path = f'{self.output_uri}/trainer/final-model.ckpt'
@@ -180,37 +239,24 @@ class RVLightning:
             ckpt = torch.load(buffer)
         state_dict = ckpt["state_dict"]
         state_dict = fix_keys(state_dict)
-        deeplab = deeplabv3_resnet50(num_classes=len(self.cc) + 1)
+        deeplab = self.model_type()(num_classes=len(self.cc) + 1)
         deeplab.load_state_dict(state_dict)
-        model = SemanticSegmentation(deeplab)
-        pds = self.build_pred_ds()
-        pred_dl = DataLoader(pds, batch_size=8, num_workers=4)
-        predictions = self.prediction_iterator(pred_dl, model, kw.get("num_preds"))
-        pred_labels = SemanticSegmentationLabels.from_predictions(
-            pds.windows,
+        model = ObjectDetection(deeplab)
+        return model
+    
+    def predict(self):
+        kw = self.kw.get("predict_kw", {})
+        model = self.reconstruct_model()
+        pred_dl = self.build_pred_loader()
+        predictions = self.prediction_iterator(pred_dl, model, kw.get("max_images", None))
+        pred_labels = ObjectDetectionLabels.from_predictions(
+            pred_dl.dataset.windows,
             predictions,
-            smooth=True,
-            extent=pds.scene.extent,
-            num_classes=len(self.cc)+1
         )
-        if kw.get("polygon_output", True):
-            voutputs = [
-                PolygonVectorOutputConfig(
-                    class_id=0,
-                    uri=f"{self.output_uri}/pred-labels-vectors/{0}.geojson",
-                    denoise=kw.get("denoise", 10),
-                ) 
-            ]
-        else:
-            voutputs = []
         pred_labels.save(
-            uri=f"{self.output_uri}/pred-labels-scores-vectors",
-            crs_transformer=pds.scene.raster_source.crs_transformer,
+            uri=f"{self.output_uri}/pred-labels",
+            crs_transformer=pred_dl.dataset.scene.raster_source.crs_transformer,
             class_config=self.cc,
-            discrete_output=False,
-            smooth_output=False,
-            smooth_as_uint8=False,
-            vector_outputs=voutputs,
         )
         return pred_labels
     
@@ -228,9 +274,8 @@ def run(config_path):
         conf
     )
 
-    # obj.train()
-
-    return obj.predict()
+    obj.train()
+    obj.predict()
 
 if __name__ == "__main__":
     import sys
