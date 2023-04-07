809d51793a20bd5517957ded5f0e33f59c12b75e
Jack Wolf <jackw@aerotractone.com>
2023-04-07 14:16:33 -0700

    work

--- rvlightning.py
+++ rvlightning.py
@@ -64,17 +64,10 @@ class ObjectDetection(pl.LightningModule):
 
     def validation_step(self, batch, batch_ind):
         x, y = batch
-        outs = self.backbone(x)
-        ys = self.to_device(y, 'cpu')
-        outs = self.to_device(outs, 'cpu')
-        return {'ys': ys, 'outs': outs}
-
-    def on_validation_batch_end(self, out, batch, batch_idx):
-        outs = self.output_to_numpy(out["outs"])
-        ys = self.output_to_numpy(out["ys"])
-        num_class_ids = 2
-        metrics = {"val_loss": 1}
-        return metrics
+        loss_dict = self.backbone(x, y)
+        loss_dict['loss'] = sum(loss_dict.values())
+        self.log("val_loss", loss_dict["loss"])
+        return loss_dict
     
     def predict(self, x, raw_out=False, out_shape=None):
         self.backbone.eval()
@@ -112,16 +105,6 @@ class RVLightning:
         
     def build_train_ds(self):
         kw = self.kw.get("train_data_kw", {})
-        # train_ds = ObjectDetectionRandomWindowGeoDataset.from_uris(
-        #     class_config=self.cc,
-        #     image_uri=self.train_uris[0],
-        #     aoi_uri=self.train_uris[2],
-        #     label_vector_uri=self.train_uris[1],
-        #     label_vector_default_class_id=self.cc.get_class_id('DF'),
-        #     size_lims=kw.get("size_lims", [300,350]),
-        #     out_size=kw.get("out_size", 325),
-        #     max_windows=kw.get("max_windows", None),
-        # )
         train_ds = ObjectDetectionSlidingWindowGeoDataset.from_uris(
             class_config=self.cc,
             image_uri=self.train_uris[0],
